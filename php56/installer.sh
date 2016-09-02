ba3c0de26ae6af28487e718176ddfe9bfea18dea
Pedro Pereira <pedromdspereira.93@gmail.com>
2016-09-02 17:06:22 +0100

    Removed nginx from php image

--- php56/installer.sh
+++ php56/installer.sh
@@ -1,14 +1,5 @@
 # Update Package List
 apt-get update
-apt-get upgrade -y
-
-# Force Locale
-echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
-locale-gen en_US.UTF-8
-export LANG=en_US.UTF-8
-
-# May the clear be with you
-echo 'printf "\033c"' >> /usr/bin
 
 # Basic packages
 apt-get install -y sudo software-properties-common nano curl \
@@ -16,7 +7,6 @@ apt-get install -y sudo software-properties-common nano curl \
     make python2.7-dev python-pip re2c supervisor unattended-upgrades whois vim zip unzip
 
 # PPA Repositories
-apt-add-repository ppa:ondrej/php5-5.6 -y
 apt-add-repository ppa:phalcon/stable  -y
 apt-get update  -y
 
@@ -38,15 +28,8 @@ echo "xdebug.var_display_max_data = -1" >> /etc/php5/mods-available/xdebug.ini
 echo "xdebug.max_nesting_level = 500" >> /etc/php5/mods-available/xdebug.ini
 
 # PHP Frameworks
+cd /
 git clone --depth=1 git://github.com/phalcon/cphalcon.git
 cd cphalcon/build
 ./install
-echo "extension=phalcon.so" >> /etc/php5/mods-available/phalcon.ini
-ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/fpm/conf.d/30-phalcon.ini
-ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/cli/conf.d/30-phalcon.ini
-
-# Install Nginx
-apt-get -y install nginx
-mkdir /var/www
-chown -R www-data:www-data /var/www
-chmod 755 /var/www
+echo "extension=phalcon.so" >> /usr/local/etc/php/conf.d/phalcon.ini
